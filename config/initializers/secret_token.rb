07dcad62e0fed37a2988ecf26904b3530a26b7b0
Matt Chun-Lum <mchunlum@gmail.com>
2013-05-28 22:45:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Papercode::Application.config.secret_token = 'f4783125e6d1b3918d46560fa3eb51b5a53f64261ab3e1c4d1f8e2a82db4672a99f883cdb92d16ed0d1ff2a57a8e63ed559a7302b283a58d24b3f386756d84c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Papercode::Application.config.session_store :cookie_store, key: '_papercode_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Papercode::Application.config.session_store :active_record_store
