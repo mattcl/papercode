0e3a1b94741a1e92b257746633390157e53210bf
Matt Chun-Lum <mchunlum@gmail.com>
2013-06-02 02:04:25 -0700

    add copy of database.yml to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,10 +29,14 @@ set :rails_env, 'production'
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
+namespace :deploy do
 #   task :start do ; end
 #   task :stop do ; end
 #   task :restart, :roles => :app, :except => { :no_release => true } do
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
-# end
+
+  task :copy_database_yml do
+    run "cp #{shared_path}/databasae.yml #{release_path}/config/database.yml"
+  end
+end
